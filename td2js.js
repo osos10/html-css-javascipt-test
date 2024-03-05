872bf2b388719d8d7d860c68debda511609424b6
osos10 <69704484+osos10@users.noreply.github.com>
2024-03-05 16:31:54 +0000

    sqrt problem solve

--- td2js.js
+++ td2js.js
@@ -86,8 +86,10 @@ function calculateFunction(func, args) {
         case 'tan':
         case 'sqrt':
         case 'exp':
-        case 'square':
+        case '√':
         return Math[func](parseFloat(args)).toFixed(2);
+        case '√':
+        return Math['sqrt'](parseFloat(args)).toFixed(2);
         case 'ln': 
         return Math['log'](parseFloat(args)).toFixed(2);
         case 'log': 
